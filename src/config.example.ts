882d2a0ba353975a7d5a185d7d43b6c92fd83dec
TheCrow95T <85043418+TheCrow95T@users.noreply.github.com>
2024-10-08 13:51:33 +0800

    added config.example.ts as template

--- src/config.example.ts
+++ src/config.example.ts
@@ -1,6 +1,9 @@
+// During setup, please use this file as the template for config.ts!
+
+
 // root url for backend
-const API_LINK = 'http://localhost:8000';
-export const API_KEY = "98rwfciuesyfaqcdnyre7vs9w8hvfw9fh";
+const API_LINK = 'http://localhost:8000'; // change to your backend api host
+export const API_KEY = "uiw9284hgvb04whv3b98"; // change to your backend api key
 
 export const LOGIN_API_URL = API_LINK + '/api/login';
 export const DATA_API_URL = API_LINK + '/api/data';
