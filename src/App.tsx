40ddd354e9297e0ba0466bfd121c117def3776fa
TheCrow95T <85043418+TheCrow95T@users.noreply.github.com>
2024-10-23 17:42:03 +0800

    added sidebar shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 import getCookie from "./utilities/getCookie";
 import NavBar from "./components/NavBar";
@@ -15,48 +16,63 @@ import PatientList from "./pages/PatientList";
 import PatientManagement from "./pages/PatientManagement";
 
 function App() {
-    const [user, setUser] = useState(getCookie("accessToken"));
-    const [outletList, setOutletList] = useState([]);
-    const [timeslotList, setTimeslotList] = useState([]);
+  const [user, setUser] = useState(getCookie("accessToken"));
+  const [outletList, setOutletList] = useState([]);
+  const [timeslotList, setTimeslotList] = useState([]);
 
-    useEffect(() => {
-        if (user) {
-            const rehabDataUponLogin = async () => {
-                const rehabCenterData = await getRehabCenterData();
-                if (rehabCenterData) {
-                    setOutletList(rehabCenterData.outlet_list);
-                    setTimeslotList(rehabCenterData.time_slot);
-                }
-            };
-            rehabDataUponLogin();
-        } else {
-            setOutletList([]);
-            setTimeslotList([]);
+  useEffect(() => {
+    if (user) {
+      const rehabDataUponLogin = async () => {
+        const rehabCenterData = await getRehabCenterData();
+        if (rehabCenterData) {
+          setOutletList(rehabCenterData.outlet_list);
+          setTimeslotList(rehabCenterData.time_slot);
         }
-    }, [user]);
+      };
+      rehabDataUponLogin();
+    } else {
+      setOutletList([]);
+      setTimeslotList([]);
+    }
+  }, [user]);
 
-    return (
-        <>
-            <div className={user ? "main" : ""}>
-                {user ? <NavBar setUser={setUser} /> : null}
-                <div className={user ? "page" : ""}>
-                    <Routes>
-                        <Route element={<LoginRoute redirectPath="/" />}>
-                            <Route path={"/login"} element={<Login setUser={setUser} />} />
-                        </Route>
-                        <Route element={<ProtectedRoute redirectPath="/login" />}>
-                            <Route path={"/"} element={<Dashboard outletList={outletList} timeslotList={timeslotList} />} />
-                            <Route path={"/patient"} element={<PatientList/>} />
-                            <Route path={"/patient/create"} element={<CreatePatient />} />
-                            <Route path={"/patient/:id"} element={<PatientManagement outletList={outletList} timeslotList={timeslotList} />} />
-                            <Route path={"/reports"} element={<Reports />} />
-                            <Route path="*" element={<p>Page not found 404</p>} />
-                        </Route>
-                    </Routes>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <SidebarProvider>
+      {user ? <NavBar setUser={setUser} /> : null}
+      {user ? <SidebarTrigger className="sidebarTrigger" /> : null}
+      <div className={user ? "page" : ""}>
+        <Routes>
+          <Route element={<LoginRoute redirectPath="/" />}>
+            <Route path={"/login"} element={<Login setUser={setUser} />} />
+          </Route>
+          <Route element={<ProtectedRoute redirectPath="/login" />}>
+            <Route
+              path={"/"}
+              element={
+                <Dashboard
+                  outletList={outletList}
+                  timeslotList={timeslotList}
+                />
+              }
+            />
+            <Route path={"/patient"} element={<PatientList />} />
+            <Route path={"/patient/create"} element={<CreatePatient />} />
+            <Route
+              path={"/patient/:id"}
+              element={
+                <PatientManagement
+                  outletList={outletList}
+                  timeslotList={timeslotList}
+                />
+              }
+            />
+            <Route path={"/reports"} element={<Reports />} />
+            <Route path="*" element={<p>Page not found 404</p>} />
+          </Route>
+        </Routes>
+      </div>
+    </SidebarProvider>
+  );
 }
 
 export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,31 +1,68 @@
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import "./Navbar.css";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+} from "@/components/ui/sidebar";
+import { Button } from "@/components/ui/button";
 
 type NavBarProp = {
-    setUser: (Status: string) => void;
+  setUser: (Status: string) => void;
 };
 
 const NavBar = ({ setUser }: NavBarProp) => {
-    const navigate = useNavigate();
-    const logout = () => {
-        const confirmLogout = confirm("Confirm Logout?");
+  const navigate = useNavigate();
+  const logout = () => {
+    const confirmLogout = confirm("Confirm Logout?");
 
-        if (confirmLogout) {
-            document.cookie = "accessToken=; path=/;";
-            navigate("/login");
-            setUser("");
-        }
-    };
-    return (
-        <div className="navbar">
-            <div className="title">Rehab Center</div>
-            <Link to={"/"}>Dashboard</Link>
-            <Link to={"/patient"}>Patient Management</Link>
-            <Link to={"/reports"}>Report</Link>
-            <div onClick={logout}>Logout</div>
-        </div>
-    );
+    if (confirmLogout) {
+      document.cookie = "accessToken=; path=/;";
+      navigate("/login");
+      setUser("");
+    }
+  };
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Rehab Center</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <Link to={"/"}>
+                  <SidebarMenuButton>Dashboard</SidebarMenuButton>
+                </Link>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <Link to={"/patient"}>
+                  <SidebarMenuButton>Patient Management</SidebarMenuButton>
+                </Link>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <Link to={"/reports"}>
+                  <SidebarMenuButton>Report</SidebarMenuButton>
+                </Link>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <Button variant="outline" onClick={logout}>
+            Logout
+          </Button>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
 };
 
 export default NavBar;
